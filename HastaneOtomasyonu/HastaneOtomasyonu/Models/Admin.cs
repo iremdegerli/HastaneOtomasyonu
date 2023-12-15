e80540f7a6747255bd3dc18008226c0895ae44d8
iremdegerli <104104302+iremdegerli@users.noreply.github.com>
2023-12-15 15:09:44 +0300

    Modeller oluşturuldu

---
+++ HastaneOtomasyonu/HastaneOtomasyonu/Models/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HastaneOtomasyonu.Models
+{
+    [Table("Admin")]
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+      
+        [Required(ErrorMessage = "Mail boş olmaz")]
+        [Display(Name = "AdminMail")]
+        [DataType(DataType.EmailAddress)]
+        public string? AdminMail { get; set; }
+       
+        [Required(ErrorMessage = "şifre boş olmaz")]
+        [Display(Name = "AdminPassword")]
+        [DataType(DataType.Password)]
+        public int AdminPassword { get; set; }
+
+    }
+}
---
+++ HastaneOtomasyonu/HastaneOtomasyonu/Models/CalismaSaati.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HastaneOtomasyonu.Models
+{
+    [Table("CalismaSaati")]
+    public class CalismaSaati
+    {
+        [Key]
+        public int SaatID { get; set; }
+
+        [Required(ErrorMessage = "İsim boş olmaz")]
+        [Display(Name = "Doktorİsmi")]
+        public string? DoktorlarAd { get; set; }
+
+        [Required(ErrorMessage = "Soyad boş olmaz")]
+        [Display(Name = "DoktorSoyad")]
+        public string? DoktorlarSoyad { get; set; }
+
+        [Required(ErrorMessage = "gün bos olmaz")]
+        [Display(Name = "calismaGunu")]
+        public string? CalismaGunu { get; set; }
+
+        public int BaslangicSaati { get; set; }
+
+        public int BitisSaati { get; set; }
+    }
+}
