9816dabb23a2cfec04d60080b059be8c61b16569
iremdegerli <104104302+iremdegerli@users.noreply.github.com>
2023-12-26 01:08:24 +0300

    Randevu sayfası oluşturuldu.

---
+++ HastaneOtomasyonu/Controllers/Randevu.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneOtomasyonu.Controllers
+{
+	public class Randevu : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
