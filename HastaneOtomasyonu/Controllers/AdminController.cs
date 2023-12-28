5468f3399a89b30e9d3308c802d026264deb111c
iremdegerli <104104302+iremdegerli@users.noreply.github.com>
2023-12-28 19:07:51 +0300

    Doktor ve poliklinik ekleme işlemleri tamamlandı.

--- HastaneOtomasyonu/Controllers/AdminController.cs
+++ HastaneOtomasyonu/Controllers/AdminController.cs
@@ -1,12 +1,93 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HastaneOtomasyonu.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HastaneOtomasyonu.Controllers
 {
-    public class AdminController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class AdminController : Controller
+	{
+		private readonly HastaneCS hastaneCS;
+
+		public AdminController (HastaneCS hastaneCS)
+		{
+			this.hastaneCS = hastaneCS;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Doktor()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Doktor(Doktorlar ekleDoktor)
+		{
+			var doktor = new Doktorlar()
+			{
+				DoktorlarId = ekleDoktor.DoktorlarId,
+				DoktorlarAd = ekleDoktor.DoktorlarAd,
+				DoktorlarSoyad = ekleDoktor.DoktorlarSoyad,
+				DoktorlarMail = ekleDoktor.DoktorlarMail,
+				DoktorlarBolum = ekleDoktor.DoktorlarBolum,
+			};
+
+			await hastaneCS.Doktorlars.AddAsync(doktor);
+			await hastaneCS.SaveChangesAsync();
+			ViewBag.correct = "Doktor başarıyla eklendi!";
+			return View("Doktor");
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> DoktorListele()
+		{
+			var doktorlar = await hastaneCS.Doktorlars.ToListAsync();
+			return View(doktorlar);
+		}
+
+		[HttpGet]
+		public IActionResult Poliklinik()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Poliklinik(Poliklinik eklePoliklinik)
+		{
+
+			var poliklinik = new Poliklinik()
+			{
+				Poliklinid = eklePoliklinik.Poliklinid,
+				PoliklinikAd = eklePoliklinik.PoliklinikAd,
+			};
+
+			await hastaneCS.Poliklinikler.AddAsync(poliklinik);
+			await hastaneCS.SaveChangesAsync();
+			ViewBag.correct = "Poliklinik başarıyla eklendi!";
+			return View("Poliklinik");
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> PoliklinikListele()
+		{
+			var poliklinikler = await hastaneCS.Poliklinikler.ToListAsync();
+			return View(poliklinikler);
+		}
+
+		public IActionResult Randevu()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Hastalar()
+		{
+			var hastalar = await hastaneCS.Hastalars.ToListAsync();
+			return View(hastalar);
+		}
+
+	}
 }
--- HastaneOtomasyonu/Program.cs
+++ HastaneOtomasyonu/Program.cs
@@ -1,7 +1,25 @@
+using HastaneOtomasyonu.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+    options.Cookie.Name = "NetCoreMvc.Auth";
+    options.LoginPath = "/User/Login";
+    options.AccessDeniedPath = "/User/Login";
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("RequireUiRole", policy => policy.RequireRole("Ui"));
+});
+builder.Services.AddDbContext<HastaneCS>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -18,7 +36,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseCookiePolicy();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
